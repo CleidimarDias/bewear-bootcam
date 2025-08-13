efa8fe5a7433ab4d03b1e5598800c5335117fc30
CleidimarDias <cleidimarcdsc@gmail.com>
2025-08-13 17:24:57 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { desc } from 'drizzle-orm'
 import Image from 'next/image'
 import React from 'react'
 
 import CategorySelector from '@/components/common/category-selector'
+import Footer from '@/components/common/footer'
 import Header from '@/components/common/header'
 import ProductList from '@/components/common/product-list'
 import { db } from '@/db'
+import { productTable } from '@/db/schema'
 
 export default async function Home() {
   const products = await db.query.productTable.findMany({
@@ -14,6 +17,14 @@ export default async function Home() {
   })
 
   const categories = await db.query.categoryTable.findMany({})
+
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true
+    }
+
+  })
 
   return (
     <>
@@ -48,6 +59,8 @@ export default async function Home() {
           />
         </div>
 
+        <ProductList products={newlyCreatedProducts} title='Novos produtos' />
+        <Footer />
       </div>
     </>
   )
---
+++ src/components/common/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='bg-accent w-full gap1 p-8'>
+            <p className='text-xs font-medium'>@ 2025 Copyright BEWEAR</p>
+            <p className='text-muted-foreground text-xs font-medium'>Todos os direitos reservados</p>
+        </div>
+    )
+}
